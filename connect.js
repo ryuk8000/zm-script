ed884cb095103bd8ade8f09e2dd54c337c427574
ryuk8000 <72906258+ryuk8000@users.noreply.github.com>
2023-04-24 15:27:44 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,6 +1,17 @@
 function myFunction() {
   // Your code here
   console.log('Window has loaded!My name is prabhat');
+   const enterKeyPressEvent = new KeyboardEvent("keydown", {
+    key: "Enter",
+    code: "Enter",
+    which: 13,
+    keyCode: 13,
+    charCode: 13,
+    bubbles: true,
+  });
+
+  // Dispatch the event on the document element
+  document.dispatchEvent(enterKeyPressEvent);
 }
 
 window.addEventListener('load', myFunction);
